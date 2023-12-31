c0918c72c79444ba018288c4c3edcb3e50368ef2
Jenry Ollivierre <jenry@jenryollivierre.com>
2023-12-31 11:03:20 -0400

    WIP

--- database/migrations/2023_12_03_182614_create_media_table.php
+++ database/migrations/2023_12_03_182614_create_media_table.php
@@ -5,8 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use JennosGroup\Laramedia\Support\Config;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
