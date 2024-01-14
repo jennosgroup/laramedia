59c209b0b35f9dd048ceb86f9df104d7e4011dad
Jenry Ollivierre <jenry@jenryollivierre.com>
2024-01-14 13:20:40 -0400

    WIP

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,7 +5,3 @@ mix.js('resources/js/files-listings.js', 'public/js');
 mix.js('resources/js/files-selector.js', 'public/js');
 
 mix.css('resources/css/laramedia.css', 'public/css');
-
-mix.copy('public/js/files-listings.js', '../../laravel-apps/test-project/public/vendor/laramedia/js');
-mix.copy('public/js/files-selector.js', '../../laravel-apps/test-project/public/vendor/laramedia/js');
-mix.copy('public/css/laramedia.css', '../../laravel-apps/test-project/public/vendor/laramedia/css');
