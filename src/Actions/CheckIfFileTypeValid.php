36e2cabbce62b18d553bbaf95b90f1391e27dc22
Jenry Ollivierre <jenry@jenryollivierre.com>
2024-12-24 16:16:45 -0400

    Fixed uploader not checking file type appropriately

--- src/Actions/CheckIfFileTypeValid.php
+++ src/Actions/CheckIfFileTypeValid.php
@@ -14,15 +14,15 @@ class CheckIfFileTypeValid
      */
     public static function execute(UploadedFile $file): bool
     {
-        if (! static::validateMimeType($file)) {
-            return false;
+        if (static::validateMimeType($file)) {
+            return true;
         }
 
-        if (! static::validateExtension($file)) {
-            return false;
+        if (static::validateExtension($file)) {
+            return true;
         }
 
-        return true;
+        return false;
     }
 
     /**
